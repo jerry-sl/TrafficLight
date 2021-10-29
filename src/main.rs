aad1c5a5a35fb6f3fc6b5a38f8886c8fd6b26080
idbati <gitter0759@gmail.com>
2021-10-29 13:24:43 +0800

    Traffic light

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let red = TrafficLight::Red(30);
+    println!("Red light: {}", red.time());
+    let green = TrafficLight::Green(50);
+    println!("Gree light: {}", green.time());
+    let yellow = TrafficLight::Yellow(5);
+    println!("Yellow light: {}", yellow.time());
+}
+
+trait TrafficLightTime {
+    fn time(&self) -> u8 {
+        unimplemented!();
+    }
+}
+
+#[derive(Debug)]
+enum TrafficLight {
+    Red(u8),
+    Green(u8),
+    Yellow(u8)
+}
+
+impl TrafficLightTime  for TrafficLight  {
+    fn time(&self) -> u8 {
+        match *self {
+            TrafficLight::Red(red) => red,
+            TrafficLight::Green(green) => green,
+            TrafficLight::Yellow(yellow) => yellow,
+        }
+    }
+}
